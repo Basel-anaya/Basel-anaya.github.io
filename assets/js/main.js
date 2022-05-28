e9633aa498be8032ddcf333dafe3502be01508eb
ReverbAI <81964452+Basel-anaya@users.noreply.github.com>
2022-05-28 15:29:15 +0300

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -256,3 +256,12 @@
 		}
 
 })(jQuery);
+// When the user scrolls the page, execute myFunction
+window.onscroll = function() {myFunction()};
+
+function myFunction() {
+  var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+  var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+  var scrolled = (winScroll / height) * 100;
+  document.getElementById("myBar").style.width = scrolled + "%";
+}
